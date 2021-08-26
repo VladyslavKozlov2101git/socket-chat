30b809863ac68a54215cfa0b3a8694b6bc6938f4
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2021-08-26 16:55:57 +0300

    # 33:11

--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import socket from '../socket'
+import axios from 'axios'
 
 export default function JoinBlock() {
+    const [roomId, setRoomId] = React.useState('')
+    const [userName, setUserName] = React.useState('')
+
+    const onEnter= ()=>{
+        if (!roomId || !userName){
+            return alert("Error data")
+        }
+        axios.post('/rooms',{
+            roomId,
+            userName
+        })
+    }
+
+ 
     return (
         <div className="join-block">
-            <input type="text" placeholder="Room Id" value="" />
-            <input type="text" placeholder="Name" value="" />
-            <button className="btn btn-success">Enter</button>
+            <input 
+                type="text" 
+                placeholder="Room Id" 
+                value={roomId} 
+                onChange={e => setRoomId(e.target.value)} 
+            />
+
+            <input 
+                type="text" 
+                placeholder="Name" 
+                value={userName} 
+                onChange={e => setUserName(e.target.value)}
+            />
+
+            <button  
+                className="btn btn-success"
+                onClick={onEnter}
+            >
+                Enter
+            </button>
         </div>
     )
 }
